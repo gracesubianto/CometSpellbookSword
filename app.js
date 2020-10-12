991cbddb76eb35f80bfc93e0f29a54de1fdd24d7
gracesubianto <gracesubianto@gmail.com>
2020-10-12 17:16:26 +1300

    added win, lose, draw func

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const userScore = 0;
-const wizScore = 0;
+let userScore = 0;
+let wizScore = 0;
 const userScore_span = document.getElementById("user-score");
 const wizScore_span = document.getElementById("wizard-score");
 const scoreBoard_div = document.querySelector(".score-board");
@@ -8,16 +8,47 @@ const comet_div = document.getElementById("c");
 const book_div = document.getElementById("b");
 const sword_div = document.getElementById("s");
 
-function getWizChoice() {
-    const choices = ['c', 'b', 's'];
-    const randomNumber = console.log(Math.floor(Math.random() * 3));
-    return choices[randomNumber];
+function getWizardChoice() {
+    const wiz_choice = ['c', 'b', 's'];
+    const randNum = Math.floor(Math.random() * 3);
+    return wiz_choice[randNum];
 }
 
-console.log(getWizChoice());
+function win(user, wizard) {
+    userScore++;
+    userScore_span.innerHTML = userScore;
+    wizScore_span.innerHTML = wizScore;
+}
+
+function lose() {
+    //wizScore++;
+
+}
+
+function draw() {
+
+}
 
 function game(userChoice) {
+    const wizardChoice = getWizardChoice();
 
+    switch (userChoice + wizardChoice){
+        case "cs":
+        case "bc":
+        case "sb":
+            win(userChoice, wizardChoice);
+            break;
+        case "sc":
+        case "cb":
+        case "bs":
+            lose(userChoice, wizardChoice);
+            break;
+        case "ss":
+        case "cc":
+        case "bb":
+            draw(userChoice, wizardChoice);
+            break;
+    }
 }
 
 function main() {
